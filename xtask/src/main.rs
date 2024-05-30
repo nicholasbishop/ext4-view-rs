44c069f5a46226085ac2f0c0e795f7abb98f105a
Nicholas Bishop <nicholasbishop@google.com>
2024-05-30 11:29:30 -0400

    xtask: Add create-test-disk action
    This will be used to create test filesystems. The files will be
    committed with git-lfs, so everyone will share the same test data, and
    developers will not need to run this create-test-disk action except when
    creating a new test file.
    
    This initial version just creates an empty filesystem; a later commit
    will mount the filesystem to add directories and files.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -6,6 +6,72 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use clap::{Parser, Subcommand};
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+struct DiskParams {
+    path: PathBuf,
+    size_in_kilobytes: u32,
+}
+
+impl DiskParams {
+    fn create(&self) {
+        let uid = nix::unistd::getuid();
+        let gid = nix::unistd::getgid();
+
+        let status = Command::new("mkfs.ext4")
+            // Set the ownership of the root directory in the filesystem
+            // to the current uid/gid instead of root. That allows the
+            // mounted filesystem to be edited without root permissions,
+            // although the mount operation itself still requires root.
+            .args(["-E", &format!("root_owner={uid}:{gid}")])
+            .arg(&self.path)
+            .arg(format!("{}k", self.size_in_kilobytes))
+            .status()
+            .unwrap();
+        assert!(status.success());
+    }
+}
+
+fn create_test_data() {
+    let dir = Path::new("test_data");
+    if !dir.exists() {
+        fs::create_dir(dir).unwrap();
+    }
+
+    let path = dir.join("test_disk1.bin");
+    if !path.exists() {
+        let disk = DiskParams {
+            path: path.to_owned(),
+            size_in_kilobytes: 1024 * 64,
+        };
+        disk.create();
+        // TODO(nicholasbishop): mount the filesystem and fill it with
+        // test data.
+    }
+}
+
+#[derive(Parser)]
+struct Opt {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    /// Create files for tests.
+    ///
+    /// The test files will be committed via git-lfs, so developers
+    /// working on the repo do not typically need to run this command.
+    CreateTestData,
+}
+
 fn main() {
-    todo!()
+    let opt = Opt::parse();
+
+    match &opt.action {
+        Action::CreateTestData => create_test_data(),
+    }
 }
