7bceeabd5b66a8f33cdf2e9bbe3c5f43aaa7e2cb
Nicholas Bishop <nbishop@nbishop.net>
2024-06-02 12:52:22 -0400

    xtask: Add mount module
    Also add a lib.rs and pub-export `Mount` there.
    
    In later commits the xtask package will have multiple binaries, and
    lib.rs allows code to be shared between them.

---
+++ xtask/src/mount.rs
@@ -0,0 +1,51 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use anyhow::{bail, Result};
+use std::path::Path;
+use std::process::Command;
+use tempfile::TempDir;
+
+/// Mounted filesystem.
+///
+/// The filesystem will be unmounted on drop.
+pub struct Mount {
+    mount_point: TempDir,
+}
+
+impl Mount {
+    /// Mount a file containing a filesystem to a temporary directory.
+    ///
+    /// Mounting is a privileged operation, so this runs `sudo mount`.
+    pub fn new(fs_bin: &Path) -> Result<Self> {
+        let mount_point = TempDir::new()?;
+        let status = Command::new("sudo")
+            .args(["mount", "-o", "ro"])
+            .args([fs_bin, mount_point.path()])
+            .status()?;
+        if !status.success() {
+            bail!("mount failed");
+        }
+        Ok(Self { mount_point })
+    }
+
+    /// Get the mount point.
+    pub fn path(&self) -> &Path {
+        self.mount_point.path()
+    }
+}
+
+impl Drop for Mount {
+    fn drop(&mut self) {
+        // Ignore errors in drop.
+        let _ = Command::new("sudo")
+            .arg("umount")
+            .arg(self.mount_point.path())
+            .status();
+    }
+}
