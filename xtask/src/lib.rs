bd1fba67de311cefa9c809ef88814607c7bbf04f
Nicholas Bishop <nbishop@nbishop.net>
2024-06-02 13:07:03 -0400

    xtask: Add calc_file_sha256
    This will be used in a couple places (verifying a downloaded file,
    comparing file hashes in tests), so put it in lib.rs.

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -8,4 +8,23 @@
 
 mod mount;
 
+use anyhow::Result;
+use sha2::Digest;
+use sha2::Sha256;
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
 pub use mount::Mount;
+
+/// Calculate the SHA256 hash of the file at `path`.
+///
+/// This calculates the hash incrementally, so large files are not
+/// loaded into memory all at once.
+pub fn calc_file_sha256(path: &Path) -> Result<String> {
+    let mut file = File::open(path)?;
+    let mut hasher = Sha256::new();
+    io::copy(&mut file, &mut hasher)?;
+    let hash = hasher.finalize();
+    Ok(format!("{hash:x}"))
+}
