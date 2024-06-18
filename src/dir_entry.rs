d127b3859c2f6f57489169efad9672120b197c9f
Nicholas Bishop <nbishop@nbishop.net>
2024-06-18 12:31:08 -0400

    dir_entry: Impl Debug for DirEntry/DirEntryName/DirEntryNameBuf

--- src/dir_entry.rs
+++ src/dir_entry.rs
@@ -6,7 +6,9 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use crate::format::format_bytes_debug;
 use crate::inode::InodeIndex;
+use core::fmt::{self, Debug, Formatter};
 use core::str::Utf8Error;
 
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -20,6 +22,12 @@ impl<'a> DirEntryName<'a> {
     #[inline]
     pub fn as_str(&self) -> Result<&'a str, Utf8Error> {
         core::str::from_utf8(self.0)
+    }
+}
+
+impl<'a> Debug for DirEntryName<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        format_bytes_debug(self.0, f)
     }
 }
 
@@ -37,6 +45,12 @@ impl DirEntryNameBuf {
     }
 }
 
+impl Debug for DirEntryNameBuf {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        format_bytes_debug(self.as_bytes(), f)
+    }
+}
+
 // Manual implementation of `PartialEq` because we don't want to compare
 // the entire `data` array, only up to `len`.
 impl PartialEq<DirEntryNameBuf> for DirEntryNameBuf {
@@ -46,11 +60,36 @@ impl PartialEq<DirEntryNameBuf> for DirEntryNameBuf {
 }
 
 /// Directory entry.
-#[derive(Clone, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct DirEntry {
     /// Number of the inode that this entry points to.
     inode: InodeIndex,
 
     /// Raw name of the entry.
     name: DirEntryNameBuf,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dir_entry_debug() {
+        let src = "abc😁\n".as_bytes();
+        let expected = "abc😁\\n"; // Note the escaped slash.
+        assert_eq!(format!("{:?}", DirEntryName(src)), expected);
+
+        let mut src_vec = src.to_vec();
+        src_vec.resize(255, 0);
+        assert_eq!(
+            format!(
+                "{:?}",
+                DirEntryNameBuf {
+                    data: src_vec.try_into().unwrap(),
+                    len: src.len().try_into().unwrap(),
+                }
+            ),
+            expected
+        );
+    }
 }
