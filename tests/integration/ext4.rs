c9ed935eea6a3555483cbbd9694a389391abb70b
Nicholas Bishop <nbishop@nbishop.net>
2024-06-27 14:09:17 -0400

    Add Ext4::read_dir and tests

--- tests/integration/ext4.rs
+++ tests/integration/ext4.rs
@@ -6,7 +6,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use ext4_view::{Ext4, Ext4Error};
+use ext4_view::{Ext4, Ext4Error, PathBuf};
 
 fn load_test_disk1() -> Ext4 {
     const DATA: &[u8] = include_bytes!("../../test_data/test_disk1.bin");
@@ -67,6 +67,57 @@ fn test_read_to_string() {
     ));
     assert!(matches!(
         fs.read_to_string("\0").unwrap_err(),
+        Ext4Error::MalformedPath
+    ));
+}
+
+#[test]
+fn test_read_dir() {
+    let fs = load_test_disk1();
+
+    // Get contents of directory `/big_dir`.
+    let dir = fs
+        .read_dir("/big_dir")
+        .unwrap()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+
+    // Get the sorted list of entry names.
+    let mut entry_names: Vec<String> = dir
+        .iter()
+        .map(|e| e.file_name().as_str().unwrap().to_owned())
+        .collect();
+    entry_names.sort_unstable();
+
+    // Get the sorted list of entry paths.
+    let mut entry_paths: Vec<PathBuf> = dir.iter().map(|e| e.path()).collect();
+    entry_paths.sort_unstable();
+
+    // Get expected entry names, 0-9999.
+    let mut expected_names = vec![".".to_owned(), "..".to_owned()];
+    expected_names.extend((0u32..10_000u32).map(|n| n.to_string()));
+    expected_names.sort_unstable();
+
+    // Get expected entry paths.
+    let expected_paths = expected_names
+        .iter()
+        .map(|n| PathBuf::try_from(format!("/big_dir/{n}").as_bytes()).unwrap())
+        .collect::<Vec<_>>();
+
+    assert_eq!(entry_names, expected_names);
+    assert_eq!(entry_paths, expected_paths);
+
+    // Errors:
+    assert!(matches!(
+        fs.read_dir("not_absolute").unwrap_err(),
+        Ext4Error::NotAbsolute
+    ));
+    assert!(matches!(
+        fs.read_dir("/empty_file").unwrap_err(),
+        Ext4Error::NotADirectory
+    ));
+    assert!(matches!(
+        fs.read_dir("\0").unwrap_err(),
         Ext4Error::MalformedPath
     ));
 }
