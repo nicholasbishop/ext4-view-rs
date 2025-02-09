32b51bba72f2ac18f86e36152b2f86d43844c1c5
Nicholas Bishop <nbishop@nbishop.net>
2025-02-09 18:25:05 -0500

    Remove `impl From<CorruptKind> for Corrupt`
    Note: no changelog update, `CorruptKind` is a private type.

--- src/error.rs
+++ src/error.rs
@@ -392,12 +392,6 @@ impl PartialEq<Incompatible> for Ext4Error {
     }
 }
 
-impl From<CorruptKind> for Corrupt {
-    fn from(c: CorruptKind) -> Self {
-        Self(c)
-    }
-}
-
 impl From<CorruptKind> for Ext4Error {
     fn from(c: CorruptKind) -> Self {
         Self::Corrupt(Corrupt(c))
