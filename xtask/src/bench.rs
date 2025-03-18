9f23380420f8142c496f1a260934938d139fb523
Nicholas Bishop <nbishop@nbishop.net>
2025-03-17 22:14:40 -0400

    Add VM benchmark
    This extends the `xtask bench` action to also run a benchmark inside a
    VM. This takes the form of a simple UEFI application.
    
    The big difference between this and the "host" benchmark is that the VM
    disk driver isn't doing any fancy caching or coalescing of reads. So it
    is currently a good bit slower than the host bench.
    
    The VM benchmark is also pretty naive in terms of the measurement. The
    measurement is done outside the VM, because it's not trivial to access a
    high-res clock in a UEFI app in a VM. The measurement includes QEMU
    startup/shutdown time. This might be improved in the future; for now I'm
    just doing the simplest thing that works.

--- xtask/src/bench.rs
+++ xtask/src/bench.rs
@@ -10,11 +10,17 @@ mod walk;
 
 use anyhow::Result;
 use ext4_view::Ext4;
-use std::path::Path;
+use ovmf_prebuilt::{Arch, FileType, Prebuilt, Source};
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 use std::time::SystemTime;
+use xtask::run_cmd;
 
 /// Run a simple wall-time performance benchmark.
 pub fn run_bench(path: &Path, iters: u32) -> Result<()> {
+    let vm_inputs = prep_vm()?;
+
     bench_impl(iters, || {
         // Load the filesystem and recursively walk all directories and
         // files. Each file is fully read and hashed.
@@ -23,6 +29,12 @@ pub fn run_bench(path: &Path, iters: u32) -> Result<()> {
         println!("filesystem hash: {digest}");
     });
 
+    bench_impl(iters, || {
+        // Run a VM with the filesystem attached as a disk. The VM runs
+        // a UEFI application which recursively walks all directories
+        // and reads each file.
+        run_vm(&vm_inputs, path).unwrap()
+    });
     Ok(())
 }
 
@@ -50,4 +62,89 @@ where
     let median = durations[durations.len() / 2];
     println!("range: {min:.2?} - {max:.2?}");
     println!("median: {median:.2?}");
+}
+
+fn run_vm(inputs: &VmInputs, filesystem: &Path) -> Result<()> {
+    let code = inputs.prebuilt.get_file(Arch::X64, FileType::Code);
+    let vars = inputs.prebuilt.get_file(Arch::X64, FileType::Vars);
+
+    let mut cmd = Command::new("qemu-system-x86_64");
+    cmd.args(["-nodefaults", "--enable-kvm"]);
+    cmd.args(["-machine", "q35"]);
+    cmd.args(["-m", "1G"]);
+    cmd.args(["-serial", "stdio"]);
+    cmd.args(["-display", "none"]);
+    cmd.args([
+        "-drive",
+        &format!(
+            "if=pflash,format=raw,readonly=on,file={}",
+            code.to_str().unwrap()
+        ),
+    ]);
+    cmd.args([
+        "-drive",
+        &format!(
+            "if=pflash,format=raw,readonly=on,file={}",
+            vars.to_str().unwrap()
+        ),
+    ]);
+    cmd.args([
+        "-drive",
+        &format!("format=raw,file=fat:rw:{}", inputs.esp.to_str().unwrap()),
+    ]);
+
+    // Add a drive for accessing the ext4 filesystem.
+    cmd.args(["-device", "virtio-scsi-pci,id=scsi"]);
+    cmd.args(["-device", "scsi-hd,drive=hd"]);
+    cmd.args([
+        "-drive",
+        &format!(
+            "if=none,id=hd,format=raw,file={}",
+            filesystem.to_str().unwrap()
+        ),
+    ]);
+
+    run_cmd(&mut cmd)?;
+
+    Ok(())
+}
+
+struct VmInputs {
+    esp: PathBuf,
+    prebuilt: Prebuilt,
+}
+
+fn prep_vm() -> Result<VmInputs> {
+    build_uefibench()?;
+    let esp = build_esp()?;
+
+    let prebuilt = Prebuilt::fetch(Source::LATEST, "target/ovmf")
+        .expect("failed to update prebuilt");
+
+    Ok(VmInputs { esp, prebuilt })
+}
+
+fn build_uefibench() -> Result<()> {
+    let mut cmd = Command::new("cargo");
+    cmd.args([
+        "build",
+        "--target",
+        "x86_64-unknown-uefi",
+        "--release",
+        "-p",
+        "uefibench",
+    ]);
+    run_cmd(&mut cmd)
+}
+
+fn build_esp() -> Result<PathBuf> {
+    let esp = Path::new("target/esp");
+    let boot = esp.join("efi/boot");
+    fs::create_dir_all(&boot)?;
+    fs::copy(
+        "target/x86_64-unknown-uefi/release/uefibench.efi",
+        boot.join("bootx64.efi"),
+    )?;
+
+    Ok(esp.to_owned())
 }
