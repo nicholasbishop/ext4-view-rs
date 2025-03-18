b001fce6e1c6eec85cc46a21abe345c0e97124cb
Nicholas Bishop <nbishop@nbishop.net>
2025-03-18 00:17:21 -0400

    checkpoint

--- benchtool/src/main.rs
+++ benchtool/src/main.rs
@@ -1,9 +1,67 @@
 #![no_main]
 #![no_std]
 
-use uefi::Status;
+extern crate alloc;
+
+use alloc::boxed::Box;
+use core::error::Error;
+use ext4_view::{Ext4, Ext4Read};
+use uefi::boot::{self, ScopedProtocol};
+use uefi::proto::media::block::BlockIO;
+use uefi::proto::media::disk::DiskIo;
+use uefi::{Handle, Status, println};
+
+struct Disk {
+    media_id: u32,
+    io: ScopedProtocol<DiskIo>,
+}
+
+impl Ext4Read for Disk {
+    fn read(
+        &mut self,
+        start_byte: u64,
+        dst: &mut [u8],
+    ) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+        Ok(self
+            .io
+            .read_disk(self.media_id, start_byte, dst)
+            .map_err(|err| Box::new(err))?)
+    }
+}
+
+fn get_media_id(handle: Handle) -> uefi::Result<u32> {
+    let bio = boot::open_protocol_exclusive::<BlockIO>(handle)?;
+    println!("last lba: {}", bio.media().last_block());
+    Ok(bio.media().media_id())
+}
 
 #[uefi::entry]
 fn main() -> Status {
-    todo!("")
+    // Find all diskio devices
+    let handles = boot::find_handles::<DiskIo>().unwrap();
+    println!("{}", handles.len());
+
+    for handle in handles {
+        let media_id = if let Ok(media_id) = get_media_id(handle) {
+            media_id
+        } else {
+            continue;
+        };
+
+        if let Ok(io) = boot::open_protocol_exclusive::<DiskIo>(handle) {
+            match Ext4::load(Box::new(Disk { media_id, io })) {
+                Ok(_fs) => {
+                    println!("open");
+                    return Status::SUCCESS;
+                }
+                Err(err) => {
+                    println!("fs err: {err}");
+                }
+            }
+        } else {
+            println!("failed to open");
+        }
+    }
+
+    panic!("failed to open ext4 filesystem");
 }
