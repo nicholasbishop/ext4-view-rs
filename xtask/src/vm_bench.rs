50f2eddb709df6e0b3f41572a1505da40bc53f0b
Nicholas Bishop <nbishop@nbishop.net>
2025-03-17 23:46:15 -0400

    checkpoint

--- xtask/src/vm_bench.rs
+++ xtask/src/vm_bench.rs
@@ -6,15 +6,84 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use ovmf_prebuilt::{Arch, FileType, Prebuilt, Source};
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn run_cmd(cmd: &mut Command) -> Result<()> {
+    println!("{cmd:?}");
+    let status = cmd.status()?;
+    if status.success() {
+        Ok(())
+    } else {
+        bail!("command failed: {status}");
+    }
+}
+
+fn build_benchtool() -> Result<()> {
+    let mut cmd = Command::new("cargo");
+    cmd.args([
+        "build",
+        "--target",
+        "x86_64-unknown-uefi",
+        "--release",
+        "-p",
+        "benchtool",
+    ]);
+    run_cmd(&mut cmd)
+}
+
+fn build_esp() -> Result<PathBuf> {
+    let esp = Path::new("target/esp");
+    let boot = esp.join("efi/boot");
+    fs::create_dir_all(&boot)?;
+    fs::copy(
+        "target/x86_64-unknown-uefi/release/benchtool.efi",
+        boot.join("bootx64.efi"),
+    )?;
+
+    Ok(esp.to_owned())
+}
 
 pub fn run_vm_bench() -> Result<()> {
+    build_benchtool()?;
+    let esp = build_esp()?;
+
     let prebuilt = Prebuilt::fetch(Source::LATEST, "target/ovmf")
         .expect("failed to update prebuilt");
 
-    let _code = prebuilt.get_file(Arch::X64, FileType::Code);
-    let _vars = prebuilt.get_file(Arch::X64, FileType::Vars);
+    let code = prebuilt.get_file(Arch::X64, FileType::Code);
+    let vars = prebuilt.get_file(Arch::X64, FileType::Vars);
+
+    let mut cmd = Command::new("qemu-system-x86_64");
+    cmd.args(["-nodefaults", "--enable-kvm"]);
+    cmd.args(["-machine", "q35"]);
+    cmd.args(["-m", "1G"]);
+    // TODO
+    //cmd.args(["-serial", "stdio"]);
+    cmd.args(["-vga", "std"]);
+    cmd.args([
+        "-drive",
+        &format!(
+            "if=pflash,format=raw,readonly=on,file={}",
+            code.to_str().unwrap()
+        ),
+    ]);
+    cmd.args([
+        "-drive",
+        &format!(
+            "if=pflash,format=raw,readonly=on,file={}",
+            vars.to_str().unwrap()
+        ),
+    ]);
+    cmd.args([
+        "-drive",
+        &format!("format=raw,file=fat:rw:{}", esp.to_str().unwrap()),
+    ]);
+
+    run_cmd(&mut cmd)?;
 
     todo!();
 }
