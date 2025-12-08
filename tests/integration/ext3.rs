4d7c6a4287145ff40e853f2823fa7a96338a4b52
Nicholas Bishop <nbishop@nbishop.net>
2025-12-07 22:33:04 -0500

    Add test_tea_htree
    This commit also updates the ext3 test disk.

--- tests/integration/ext3.rs
+++ tests/integration/ext3.rs
@@ -7,13 +7,30 @@
 // except according to those terms.
 
 use crate::test_util::load_compressed_filesystem;
+use ext4_view::{Ext4, Path};
+
+pub fn load_ext3() -> Ext4 {
+    load_compressed_filesystem("test_disk_ext3.bin.zst")
+}
 
 /// Test reading an inode from a filesystem with the minimum inode size
 /// of 128 bytes.
 #[test]
 fn test_read_small_inode() {
-    let fs = load_compressed_filesystem("test_disk_ext3.bin.zst");
+    let fs = load_ext3();
     let mut dir_iter = fs.read_dir("/").unwrap();
     let entry = dir_iter.next().unwrap().unwrap();
     assert_eq!(entry.file_name(), ".");
+}
+
+/// Test reading files from an htree directory that uses TEA hashes.
+#[test]
+fn test_tea_htree() {
+    let fs = load_ext3();
+
+    let medium_dir = Path::new("/medium_dir");
+    for i in 0..1_000 {
+        let i = i.to_string();
+        assert_eq!(fs.read_to_string(&medium_dir.join(&i)).unwrap(), i);
+    }
 }
